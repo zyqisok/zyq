0301660f2430804d6a3078ff196f375965e7d976
zyq <934474638@qq.com>
2019-04-21 21:56:42 +0800

    提交登录页面

--- src/main/java/com/zyq/controller/UserController.java
+++ src/main/java/com/zyq/controller/UserController.java
@@ -3,7 +3,6 @@ package com.zyq.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.zyq.beans.User;
 import com.zyq.service.UserService;
@@ -19,13 +18,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/add")
-    @ResponseBody
+    @RequestMapping("/toLogin")
     public String index(){
         User user = new User();
         user.setName("zyq");
         userService.save(user);
-        return "this is user index";
+        return "user/login";
     }
 
 }
